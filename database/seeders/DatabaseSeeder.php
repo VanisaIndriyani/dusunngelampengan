12d1a5f2666a11a33874e707aa08288094681f26
Vanisa Indiyani <vanisaindriyani30@gmail.com>
2025-08-17 11:22:44 +0700

    Pesan commit yang menjelaskan perubahan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
-            RTSeeder::class,
-            UMKMSeeder::class,
+         
             ContentSeeder::class,
         ]);
     }
